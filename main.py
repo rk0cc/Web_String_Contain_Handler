7c386529c81e54a054ca65ae56c9219d2e32fd1d
Cyrus Chan <cyruschan1212@gmail.com>
2020-11-23 21:54:56 +0800

    Add requirement.txt

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import unicode_string_contain_check as uscc
 
-if __name__ == "__main__":
+import sys
+from getopt import getopt
+
+def main(argv):
     pass
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
