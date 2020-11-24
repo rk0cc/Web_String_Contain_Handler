0d1ccf24b7ffc4500e772588ca1aeb38ad2f207f
Cyrus Chan <cyruschan1212@gmail.com>
2020-11-24 15:46:15 +0800

    Change display on HTML

--- handler.php
+++ handler.php
@@ -14,22 +14,33 @@
         //When running in Windows
         exec("python main.py -p ".$phrase." -k ".$keyword,$CONSOLE_LOG,$USCC_EXIT_CODE);
     }
-    
-    //Decition for exit code depending executions
-    switch($USCC_EXIT_CODE) {
-        case 0:
-            //Contains
-            echo("This phrase is containing keyword");
-            break;
-        case 1:
-            //Not contains
-            echo("This phrase does not contain keyword");
-            break;
-        default:
-            //Unexpected error
-            echo("No doubt that you inputed invalid data here:\n");
-            var_dump($CONSOLE_LOG);
-            break;
-    }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>USCC Result</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                //Decition for exit code depending executions
+                switch($USCC_EXIT_CODE) {
+                    case 0:
+                        //Contains
+                        echo("This phrase is containing keyword");
+                        break;
+                    case 1:
+                        //Not contains
+                        echo("This phrase does not contain keyword");
+                        break;
+                    default:
+                        //Unexpected error
+                        echo("No doubt that you inputed invalid data!!!!");
+                        break;
+                }
+            ?>
+        </h1>
+    </body>
+</html>
 
