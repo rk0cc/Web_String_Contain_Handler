79adb8c9ec28700ad3399ee0ad392f6816877328
Cyrus Chan <cyruschan1212@gmail.com>
2020-11-24 15:35:28 +0800

    Add comment

--- handler.php
+++ handler.php
@@ -1,22 +1,32 @@
 <?php
+    //Get phrase
     $phrase = $_POST["usr_input"];
+    //Get keyword
     $keyword = $_POST["target_text"];
+    //Get output from program
     $CONSOLE_LOG;
+    //Store exit code for condition of checking result
     $USCC_EXIT_CODE;
-    if (PHP_OS==="Linux") {
+    if (PHP_OS_FAMILY!=="Windows") {
+        //When user using non-Windows system
         exec("python3 main.py -p ".$phrase." -k ".$keyword,$CONSOLE_LOG,$USCC_EXIT_CODE);
     } else {
+        //When running in Windows
         exec("python main.py -p ".$phrase." -k ".$keyword,$CONSOLE_LOG,$USCC_EXIT_CODE);
     }
     
+    //Decition for exit code depending executions
     switch($USCC_EXIT_CODE) {
         case 0:
+            //Contains
             echo("This phrase is containing keyword");
             break;
         case 1:
+            //Not contains
             echo("This phrase does not contain keyword");
             break;
         default:
+            //Unexpected error
             echo("No doubt that you inputed invalid data here:\n");
             var_dump($CONSOLE_LOG);
             break;
